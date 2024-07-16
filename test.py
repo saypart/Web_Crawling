bb4772d928046e3d7b6bfb8af484b4cd0cf6122d
sanghyeon <saypart@gmail.com>
2024-07-16 16:29:57 +0900

    파이썬을 이용한 웹 크롤링 학습

---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas     #
+import selenium   # 웹페이지 자동화 툴
+import bs4        # 웹 스크래핑 및 데이터 추출
+import openpyxl  # 엑셀파일을 불러오고 데이터를 입력
+import selenium.webdriver   
+
+# 크롬 web driver 객체 생성
+selenium.webdriver.Chrome()
